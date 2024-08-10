3d319e4138d35aaeb42787eb5006bb6c72989281
D. Bohdan <dbohdan@dbohdan.com>
2024-08-10 08:30:06 +0000

    refactor: represent link types as classes
    This makes the code more verbose but IMO easier to follow.

--- gmi2md.php
+++ gmi2md.php
@@ -1,46 +1,144 @@
 #! /usr/bin/env php
 <?php declare(strict_types=1);
 
-enum LineType: string
-{
-    case Blank = 'blank';
-    case Blockquote = 'blockquote';
-    case Heading = 'heading';
-    case Link = 'link';
-    case List = 'list';
-    case Paragraph = 'paragraph';
-    case Preformatted = 'preformatted';
-    case PreformattedToggle = 'preformatted-toggle';
+interface LineTypeInterface {
+    public static function isType(string $line): bool;
+    public static function convert(string $line): string;
 }
 
-final class GemtextToMarkdownConverter
-{
-    private const BLOCKQUOTE_PREFIX = '>';
-    private const HEADING_REGEX = '/^(#{1,3})[ \t]*/';
-    private const HTTP = 'http';
-    private const LINK_PREFIX = '=>';
-    private const LIST_PREFIX = '* ';
-    private const PREFORMATTED_TOGGLE_PREFIX = '```';
-    private const WHITESPACE_REGEX = '/[ \t]+/';
-
-    private string $separator;
-
-    public function __construct(string $separator = '<br>')
-    {
-        $this->separator = $separator;
+final class BlankLine implements LineTypeInterface {
+    public static function isType(string $line): bool {
+        return $line === '';
     }
 
-    public function convert(string $input): string
-    {
+    public static function convert(string $line): string {
+        return '';
+    }
+}
+
+final class BlockquoteLine implements LineTypeInterface {
+    private const PREFIX = '>';
+
+    public static function isType(string $line): bool {
+        return str_starts_with($line, self::PREFIX);
+    }
+
+    public static function convert(string $line): string {
+        return self::PREFIX . htmlspecialchars(substr($line, strlen(self::PREFIX)));
+    }
+}
+
+final class HeadingLine implements LineTypeInterface {
+    private const REGEX = '/^(#{1,3})[ \t]*/';
+
+    public static function isType(string $line): bool {
+        return (bool) preg_match(self::REGEX, $line);
+    }
+
+    public static function convert(string $line): string {
+        return preg_replace(self::REGEX, '\1 ', $line);
+    }
+}
+
+final class LinkLine implements LineTypeInterface {
+    private const PREFIX = '=>';
+    private const HTTP = 'http';
+    private const WHITESPACE_REGEX = '/[ \t]+/';
+
+    public static function isType(string $line): bool {
+        return str_starts_with($line, self::PREFIX);
+    }
+
+    public static function convert(string $line): string {
+        $link = trim(substr($line, strlen(self::PREFIX)));
+
+        return str_starts_with($link, self::HTTP)
+            && !preg_match(self::WHITESPACE_REGEX, $link)
+                ? '<' . htmlspecialchars($link) . '>'
+                : self::createMarkdownLink($link);
+    }
+
+    private static function createMarkdownLink(string $link): string {
+        $parts = preg_split(self::WHITESPACE_REGEX, $link, 2);
+        $url = $parts[0];
+        $title = $parts[1] ?? $url;
+
+        return '[' . htmlspecialchars($title) . '](' . htmlspecialchars($url) . ')';
+    }
+}
+
+final class ListLine implements LineTypeInterface {
+    private const PREFIX = '* ';
+
+    public static function isType(string $line): bool {
+        return str_starts_with($line, self::PREFIX);
+    }
+
+    public static function convert(string $line): string {
+        return htmlspecialchars($line);
+    }
+}
+
+final class ParagraphLine implements LineTypeInterface {
+    public static function isType(string $line): bool {
+        // This is the default case.
+        return true;
+    }
+
+    public static function convert(string $line): string {
+        return htmlspecialchars($line);
+    }
+}
+
+final class PreformattedLine implements LineTypeInterface {
+    public static function isType(string $line): bool {
+        // This is handled separately in the main converter.
+        return false;
+    }
+
+    public static function convert(string $line): string {
+        return htmlspecialchars($line);
+    }
+}
+
+final class PreformattedToggleLine implements LineTypeInterface {
+    private const PREFIX = '```';
+
+    public static function isType(string $line): bool {
+        return str_starts_with(rtrim($line), self::PREFIX);
+    }
+
+    public static function convert(string $line): string {
+        return self::PREFIX;
+    }
+}
+
+final readonly class GemtextToMarkdownConverter {
+    private const LINE_TYPES = [
+        BlankLine::class,
+        BlockquoteLine::class,
+        HeadingLine::class,
+        LinkLine::class,
+        ListLine::class,
+        PreformattedToggleLine::class,
+        // This should be last since it's the default case.
+        ParagraphLine::class,
+    ];
+
+    public function __construct(
+        private string $separator = '<br>'
+    ) {}
+
+    public function convert(string $input): string {
         $lines = explode("\n", $input);
         $output = [];
         $inPreformatted = false;
-        $previousLineType = LineType::Blank;
+        $previousLineType = BlankLine::class;
 
         foreach ($lines as $line) {
             [$lineType, $convertedLine] = $this->convertLine($line, $inPreformatted);
 
-            if ($lineType === LineType::PreformattedToggle) {
+            if ($lineType === PreformattedToggleLine::class) {
                 $inPreformatted = !$inPreformatted;
             }
 
@@ -51,34 +149,35 @@ final class GemtextToMarkdownConverter
         return implode("\n", $output);
     }
 
-    private function convertLine(string $line, bool $inPreformatted): array
-    {
+    private function convertLine(string $line, bool $inPreformatted): array {
         $trimmedLine = rtrim($line);
 
-        if (str_starts_with($trimmedLine, self::PREFORMATTED_TOGGLE_PREFIX)) {
+        if (PreformattedToggleLine::isType($trimmedLine)) {
             return [
-                LineType::PreformattedToggle,
-                $inPreformatted ? self::PREFORMATTED_TOGGLE_PREFIX : $line,
+                PreformattedToggleLine::class,
+                $inPreformatted ? PreformattedToggleLine::convert($line) : $line,
             ];
         }
 
         if ($inPreformatted) {
-            return [LineType::Preformatted, htmlspecialchars($line)];
+            return [PreformattedLine::class, PreformattedLine::convert($line)];
         }
 
-        $lineType = $this->lineType($trimmedLine);
-        $convertedLine = match ($lineType) {
-            LineType::Blockquote => $this->convertBlockquote($trimmedLine),
-            LineType::Heading => $this->convertHeading($trimmedLine),
-            LineType::Link => $this->convertLink($trimmedLine),
-            default => htmlspecialchars($trimmedLine),
-        };
-
-        return [$lineType, $convertedLine];
+        $lineType = $this->getLineType($trimmedLine);
+        return [$lineType, $lineType::convert($trimmedLine)];
     }
 
-    private function addLineWithSpacing(array &$output, string $line, LineType $lineType, LineType $previousLineType): void
-    {
+    private function getLineType(string $line): string {
+        foreach (self::LINE_TYPES as $lineType) {
+            if ($lineType::isType($line)) {
+                return $lineType;
+            }
+        }
+
+        throw new UnexpectedValueException('Unexpected line type');
+    }
+
+    private function addLineWithSpacing(array &$output, string $line, string $lineType, string $previousLineType): void {
         if ($this->shouldPrependSeparator($lineType, $previousLineType)) {
             $line = $this->separator . $line;
         } elseif ($this->shouldAddBlankLine($lineType, $previousLineType)) {
@@ -88,82 +187,21 @@ final class GemtextToMarkdownConverter
         $output[] = $line;
     }
 
-    private function shouldAddBlankLine(LineType $currentLineType, LineType $previousLineType): bool
-    {
-        return ($currentLineType === $previousLineType &&
-            ($currentLineType === LineType::Blockquote ||
-                $currentLineType === LineType::Paragraph)) ||
-            ($currentLineType !== $previousLineType &&
-                $currentLineType !== LineType::Blank &&
-                $currentLineType !== LineType::Preformatted &&
-                $previousLineType !== LineType::Blank &&
-                $previousLineType !== LineType::Preformatted);
+    private function shouldAddBlankLine(string $currentLineType, string $previousLineType): bool {
+        return ($currentLineType === $previousLineType
+            && ($currentLineType === BlockquoteLine::class
+                || $currentLineType === ParagraphLine::class))
+            || ($currentLineType !== $previousLineType
+                && $currentLineType !== BlankLine::class
+                && $currentLineType !== PreformattedLine::class
+                && $previousLineType !== BlankLine::class
+                && $previousLineType !== PreformattedLine::class);
     }
 
-    private function shouldPrependSeparator(LineType $currentLineType, LineType $previousLineType): bool
-    {
-        return $currentLineType === LineType::Link &&
-            ($previousLineType === LineType::Link ||
-                $previousLineType === LineType::Paragraph);
-    }
-
-    private function lineType(string $line): LineType
-    {
-        return match (true) {
-            $line === '' => LineType::Blank,
-            $this->isBlockquote($line) => LineType::Blockquote,
-            $this->isHeading($line) => LineType::Heading,
-            $this->isLink($line) => LineType::Link,
-            $this->isList($line) => LineType::List,
-            default => LineType::Paragraph,
-        };
-    }
-
-    private function isHeading(string $line): bool
-    {
-        return (bool) preg_match(self::HEADING_REGEX, $line);
-    }
-
-    private function isLink(string $line): bool
-    {
-        return str_starts_with($line, self::LINK_PREFIX);
-    }
-
-    private function convertBlockquote(string $line): string
-    {
-        return self::BLOCKQUOTE_PREFIX
-            . htmlspecialchars(substr($line, strlen(self::BLOCKQUOTE_PREFIX)));
-    }
-
-    private function convertHeading(string $line): string
-    {
-        return preg_replace(self::HEADING_REGEX, '\1 ', $line);
-    }
-
-    private function convertLink(string $line): string
-    {
-        $link = trim(substr($line, strlen(self::LIST_PREFIX)));
-
-        if (str_starts_with($link, self::HTTP) &&
-                !preg_match(self::WHITESPACE_REGEX, $link)) {
-            return '<' . htmlspecialchars($link) . '>';
-        }
-
-        $parts = preg_split(self::WHITESPACE_REGEX, $link, 2);
-        $url = $parts[0];
-        $title = $parts[1] ?? $url;
-
-        return '[' . htmlspecialchars($title) . '](' . htmlspecialchars($url) . ')';
-    }
-
-    private function isList(string $line): bool
-    {
-        return str_starts_with($line, self::LIST_PREFIX);
-    }
-
-    private function isBlockquote(string $line): bool
-    {
-        return str_starts_with($line, self::BLOCKQUOTE_PREFIX);
+    private function shouldPrependSeparator(string $currentLineType, string $previousLineType): bool {
+        return $currentLineType === LinkLine::class
+            && ($previousLineType === LinkLine::class
+                || $previousLineType === ParagraphLine::class);
     }
 }
 
